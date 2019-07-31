7e9862cb51ca0be02eb6f2fec100f9ff836fe8ea
ddddddddomi <po04154@naver.com>
2019-07-31 16:36:11 +0900

    first commit

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def summary(self):
+        return self.body[:100]
+
+# Create your models here.
